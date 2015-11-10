3c0999685afad042543f2329d727d6ddf219e30e
alex-icez <alex-icex@mail.ru>
2015-11-10 20:15:08 +0300

    mess

--- src/lecture_7/Sampole3.java
+++ src/lecture_7/Sampole3.java
@@ -1,11 +1,19 @@
 package lecture_7;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 import javax.imageio.ImageIO;
 
 public class Sampole3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedImage img = ImageIO.read(new File("/home/sasha/3244317.jpg"));
+		int w = img.getWidth();
+		int h = img.getHeight();
+		
+		
 
 	}
 
