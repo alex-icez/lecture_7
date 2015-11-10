2dd096607fae9b45d40af6425c12a24ecc30a9b1
alex-icez <alex-icex@mail.ru>
2015-11-10 20:55:02 +0300

    mess

--- src/lecture_7/Sample4.java
+++ src/lecture_7/Sample4.java
@@ -5,6 +5,7 @@ public class Sample4 {
 	boolean fact(double xn, double yn, double xc, double yc, int k) {
 		double x1 = xn * xn - yn * yn + xc;
 		double y1 = 2 * xn * yn + yc;
+		double l = (x1 - xn) * (x1 - xn) + (y1 - yn) * (y1 - yn);
 		
 	}
 	
