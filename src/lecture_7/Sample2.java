216a8c64cc1ae24b8ce21ae7f6510e1a2cf890a6
alex-icez <alex-icex@mail.ru>
2015-11-10 19:29:10 +0300

    mess

--- src/lecture_7/Sample2.java
+++ src/lecture_7/Sample2.java
@@ -6,7 +6,7 @@ public class Sample2 {
 
 	public static void main(String[] args) {
 		int w = 1024, h = 1024;
-		BufferedImage img = new BufferedImage(w, h, imageType)
+		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 
 	}
 
