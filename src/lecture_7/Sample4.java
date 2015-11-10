510aa099fb7dc31b2ec12383359dc2c1d6461a14
alex-icez <alex-icex@mail.ru>
2015-11-10 21:03:52 +0300

    mess

--- src/lecture_7/Sample4.java
+++ src/lecture_7/Sample4.java
@@ -8,14 +8,14 @@ import javax.imageio.ImageIO;
 
 public class Sample4 {
 
-	static boolean fact(double xn, double yn, double xc, double yc, int k) {
+	static int fact(double xn, double yn, double xc, double yc, int k) {
 		double x1 = xn * xn - yn * yn + xc;
 		double y1 = 2 * xn * yn + yc;
 		double l = (x1 - xn) * (x1 - xn) + (y1 - yn) * (y1 - yn);
 		if (l > 100.0)
-			return false;
+			return l / (k + 1);
 		if (k > 100)
-			return true;
+			return -1;
 		return fact(x1, y1, xc, yc, k + 1);
 	}
 	
