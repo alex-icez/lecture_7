ba48288a722c8b1d8c6f0fe94a19238863bbf20c
alex-icez <alex-icex@mail.ru>
2015-11-10 20:55:36 +0300

    mess

--- src/lecture_7/Sample4.java
+++ src/lecture_7/Sample4.java
@@ -6,6 +6,9 @@ public class Sample4 {
 		double x1 = xn * xn - yn * yn + xc;
 		double y1 = 2 * xn * yn + yc;
 		double l = (x1 - xn) * (x1 - xn) + (y1 - yn) * (y1 - yn);
+		if (l > 100.0)
+			return false
+		
 		
 	}
 	
