613804c91c39b7f1643e71e62864820574d8e2d9
alex-icez <alex-icex@mail.ru>
2015-11-10 19:36:42 +0300

    mess

--- src/lecture_7/Sample2.java
+++ src/lecture_7/Sample2.java
@@ -2,12 +2,13 @@ package lecture_7;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class Sample2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int w = 1024, h = 1024;
 		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 		for(int y = 0; y < h; y++)
@@ -17,9 +18,6 @@ public class Sample2 {
 			}
 		
 		ImageIO.write(img, "png", new File("pic1.png"));
-		
-		
-
 	}
 
 }
