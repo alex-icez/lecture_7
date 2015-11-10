bdb4b789ded23ff83b8f49553e849c9c957ca6bb
alex-icez <alex-icex@mail.ru>
2015-11-10 20:54:30 +0300

    mess

--- src/lecture_7/Sample4.java
+++ src/lecture_7/Sample4.java
@@ -2,8 +2,15 @@ package lecture_7;
 
 public class Sample4 {
 
+	boolean fact(double xn, double yn, double xc, double yc, int k) {
+		double x1 = xn * xn - yn * yn + xc;
+		double y1 = 2 * xn * yn + yc;
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
