43ee386e02d4451de80219f6e2672c1e7af29352
alex-icez <alex-icex@mail.ru>
2015-11-10 19:31:20 +0300

    mess

--- src/lecture_7/Sample2.java
+++ src/lecture_7/Sample2.java
@@ -7,6 +7,13 @@ public class Sample2 {
 	public static void main(String[] args) {
 		int w = 1024, h = 1024;
 		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		img.getRGB(x, y);
+		img.setRGB(x, y, rgb);
+		
+		
+		
+		
+		
 
 	}
 
